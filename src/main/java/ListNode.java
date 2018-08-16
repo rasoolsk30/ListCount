b34d5137afe258d65089e5d9382e1f21cf04e000
rasool <rasools27@gmail.com>
2018-08-16 19:42:35 +0530

    add file

---
+++ src/main/java/ListNode.java
@@ -0,0 +1,29 @@
+
+public class ListNode {
+	public ListNode()
+	{	this(null, null);
+	}
+
+	
+	public ListNode(Object data, ListNode next)
+	{	Object myData = data;
+		ListNode myNext = next;
+	}
+
+	public Object getData()
+	{	return getData();	}
+
+	
+	public ListNode getNext()
+	{	return getNext();	}
+
+	public void setData(Object data)
+	{	Object myData = data;	}
+
+	
+	public void setNext(ListNode next)
+	{	ListNode myNext = next;	
+	}
+ 
+
+}
